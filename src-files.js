0d750b6b3a8dc30e2954c64477948d7452ab9c81
aarkegz <aarkegz@gmail.com>
2025-06-18 13:28:02 +0000

    Deploying to gh-pages from @ arceos-hypervisor/riscv_vcpu@d457d81bab48e3525ff98987b5220572f7968d47 🚀

---
+++ src-files.js
@@ -0,0 +1,3 @@
+var srcIndex = new Map(JSON.parse('[["riscv_vcpu",["",[],["consts.rs","detect.rs","guest_mem.rs","lib.rs","percpu.rs","regs.rs","sbi_console.rs","trap.rs","vcpu.rs"]]]]'));
+createSrcSidebar();
+//{"start":36,"fragment_lengths":[131]}
